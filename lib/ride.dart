60856f40dabf26709cc7a94a1532b194b02228c1
Bogdan Barbu <bogdanbarbuflorin@gmail.com>
2024-04-07 10:43:15 +0300

    update qIndex limits

--- lib/ride.dart
+++ lib/ride.dart
@@ -71,7 +71,7 @@ class _RideScreenState extends State<RideScreen> {
   }
 
   void startTracking() {
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
+    _timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
       if(firstRun) {
         position1 = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
         startingPosition = position1;
@@ -140,9 +140,9 @@ class _RideScreenState extends State<RideScreen> {
     print(points);
     Color color = Colors.green;
     for (int i = 0; i < points.length-1; i+=2) {
-      if(dataList[i ~/ 2]['qIndex'] > 5 && dataList[i ~/ 2]['qIndex'] < 7) {
+      if(dataList[i ~/ 2]['qIndex'] < -1 && dataList[i ~/ 2]['qIndex'] > -2) {
         color = Colors.orange;
-      } else if(dataList[i ~/ 2]['qIndex'] >= 7) color = Colors.red;
+      } else if(dataList[i ~/ 2]['qIndex'] <= -2) color = Colors.red;
       else color = Colors.green;
       final PolylineId polylineId = PolylineId(i.toString());
       final Polyline polyline = Polyline(
